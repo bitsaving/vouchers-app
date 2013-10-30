2c17a20d0545478ae6107020533b9b9476a5c8e7
DivyaTalwar <divya@vinsol.com>
2013-10-31 01:18:06 +0530

    review

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
+  validates :description,:presence=>true
   belongs_to :user
   belongs_to :voucher
+  
 end
--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,45 +1,35 @@
 class Voucher < ActiveRecord::Base 
-    include Workflow
-    workflow_column :workflow_state
-    workflow do
-      state :initial do
-        event :create, :transitions_to => :new
-      end
-      state :new do
-        event :send_for_approval,:transitions_to=> :pending
-      end
-      state :pending do
-        event :review, :transitions_to => :being_reviewed
-      end
-      state :being_reviewed do
-        event :approved, :transitions_to => :approved
-        event :reject, :transitions_to => :rejected
-      end
-      state :approved do
-        event :approved, :transitions_to => :accepted
-        event :reject, :transitions_to => :rejected
-      end
-      state :accepted
-      state :rejected
+  include Workflow
+  workflow_column :workflow_state
+  workflow do
+    state :new do
+      event :send_for_approval,:transitions_to=> :pending
     end
-    PAYMENT_TYPES = [ "Check", "Credit card", "Bank transfers","Cash" ]
-    has_many :comments
-    validates :date,:from_date,:to_date,:pay_type,:amount ,presence: true
-    validates :debit_from_id,:credit_to_id ,:presence=>true,:on => :create
-    validates :to_date, :date => {:after_or_equal_to => :from_date,
-      :message => 'must be after start date of project'}
-    validates :amount, :numericality => true
-    belongs_to :debit_from, :class_name => 'Account'
-    belongs_to :credit_to, :class_name => 'Account'
-    belongs_to :assigned_to ,:class_name=>'User'
-    belongs_to :user
-    has_many :uploads ,dependent: :destroy
-    accepts_nested_attributes_for :uploads, allow_destroy: true, update_only: true 
-    accepts_nested_attributes_for :comments, allow_destroy: true, update_only: true
-    after_commit :update_vouchers_status, :on => :create
-
-    def update_vouchers_status
-      self.create!
+    state :pending do
+      event :approve, :transitions_to => :approved
+      event :reject, :transitions_to => :rejected
     end
-
+    state :approved do
+      event :accept, :transitions_to => :accepted
+      event :reject, :transitions_to => :rejected
+    end
+    state :accepted
+    state :rejected do
+      event :send_for_approval ,:transitions_to => :pending
+    end
+  end
+  PAYMENT_TYPES = [ "Check", "Credit card", "Bank transfers","Cash" ]
+  has_many :comments
+  validates :date,:payment_type,:amount ,presence: true
+  validates :account_credited,:account_debited ,:presence=>true,:on => :create
+  validates :to_date, :date => {:after_or_equal_to => :from_date,
+    :message => 'must be after start date of project'}
+  validates :amount, numericality: { greater_than: 0.01}
+  belongs_to :debit_from, :class_name => 'Account', :foreign_key => "account_debited"
+  belongs_to :credit_to, :class_name => 'Account', :foreign_key => "account_credited"
+  belongs_to :assigned_to ,:class_name=>'User',:foreign_key=>"assignee_id"
+  belongs_to :creator ,:class_name=> "User"
+  has_many :uploads ,dependent: :destroy
+  accepts_nested_attributes_for :uploads, allow_destroy: true, update_only: true , reject_if: proc { |attributes| attributes['avatar'].blank? }
+  accepts_nested_attributes_for :comments, allow_destroy: true, update_only: true,reject_if: proc { |attributes| attributes['description'].blank? }
 end
