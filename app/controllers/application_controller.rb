bc6a8a04b263b4d277d8397ca61e5bb95bd65b2b
DivyaTalwar <divya@vinsol.com>
2013-11-06 00:10:49 +0530

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,14 +4,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  # before_action :authorize
-  # def render_404
-  #   respond_to do |format|
-  #     format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
-  #     format.xml  { head :not_found }
-  #     format.any  { head :not_found }
-  #   end
-  # end
+ 
  protected
    def authorize    
     redirect_to new_user_session_path, notice: "Please log in" if current_user.nil?
