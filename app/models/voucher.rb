44a8b5e252652f7e2ea845f36d7cd5b06777a336
DivyaTalwar <divya@vinsol.com>
2013-11-05 23:54:34 +0530

    changes

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -23,14 +23,14 @@ class Voucher < ActiveRecord::Base
   has_many :comments
   validates :date,:payment_type,:amount ,presence: true
   validates :account_credited,:account_debited ,:presence=>true,:on => :create
-  validates :to_date, :date => {:after_or_equal_to => :from_date,
+  validates :to_date, :date => { :after_or_equal_to => :from_date,
     :message => 'must be after start date of project'}
 
   validates :amount, numericality: { greater_than: 0.01}
   belongs_to :debit_from, :class_name => 'Account', :foreign_key => "account_debited"
   belongs_to :credit_to, :class_name => 'Account', :foreign_key => "account_credited"
-  belongs_to :assigned_to ,:class_name=>'User',:foreign_key=>"assignee_id"
-  belongs_to :creator ,:class_name=> "User"
+  belongs_to :assigned_to ,:class_name =>'User',:foreign_key=>"assignee_id"
+  belongs_to :creator ,:class_name => "User"
   has_many :uploads ,dependent: :destroy
   accepts_nested_attributes_for :uploads, update_only: true , reject_if: proc { |attributes| attributes['avatar'].blank? }, allow_destroy: true
   accepts_nested_attributes_for :comments, allow_destroy: true, update_only: true,reject_if: proc { |attributes| attributes['description'].blank? }
