9bf7c26d5ac25a05074c793a4eeb5f429e461d76
DivyaTalwar <divya@vinsol.com>
2013-11-15 13:22:10 +0530

    review

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -2,7 +2,7 @@ class Upload < ActiveRecord::Base
   belongs_to :voucher 
   has_attached_file :avatar, :url =>"/assets/vouchers/:id/:style/:basename.:extension",:path=>":rails_root/public/assets/vouchers/:id/:style/:basename.:extension"
   validates_attachment_size :avatar,:less_than =>5.megabytes
-  validates_attachment_content_type :avatar,:content_type =>'application/pdf' 
+  # validates_attachment_content_type :avatar,:content_type =>'application/pdf' 
   before_save :rename_avatar
   def rename_avatar
 	if avatar_file_name
