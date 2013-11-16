a0ad0ea1e42b3dee3504f6ee509c41edee658ee0
Akhil Bansal <bansalakhil30.10@gmail.com>
2013-11-16 16:12:40 +0530

    Added more CR comments to accounts views, comments controller and helper

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:destroy]
-  
+
   def create
+    #FIXME_AB: I think a better way to do is find voucher first and then do voucher.comments.build
     @comment = Comment.new(comment_params)
     if(@comment.save)
       respond_to do |format|
@@ -10,8 +11,9 @@ class CommentsController < ApplicationController
       end
     else
       respond_to do |format|
-        format.html do 
+        format.html do
           redirect_to :back
+          #FIXME_AB: It would be good if you can also display specific error, why comment was not saved
           flash[:notice] = "Comment could not be added"
         end
         format.js do
@@ -20,17 +22,15 @@ class CommentsController < ApplicationController
     end
   end
   private
-    # Use callbacks to share common setup or constraints between actions.
+  # Use callbacks to share common setup or constraints between actions.
   def set_comment
     @comment = Comment.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      redirect_to :back
+  rescue ActiveRecord::RecordNotFound
+    redirect_to :back
   end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+  # Never trust parameters from the scary internet, only allow the white list through.
   def comment_params
     params.permit(:description,:accepted,:voucher_id).merge({ user_id: current_user.id })
   end
 end
-
-
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,25 +16,32 @@ module ApplicationHelper
     User.where.not(id:  current_user.id).pluck( 'first_name ', 'id' )
   end
 
+  #FIXME_AB: I am not sure why we need this. A better way could be to extend time formats. We had a session by Pramod once
   def format_date(date)
     date ? date.strftime('%d-%b- %Y') : nil
   end
 
+  #FIXME_AB: I think we don't need this method. if we add a scope called pending in voucher which return pending vouchers then we can get same thing by account.vouchers.pending. No?
   def get_pending_vouchers(account_id)
+    #FIXME_AB: Since we have only one account the why we are using 'in' query
     @vouchers = Voucher.where(workflow_state: 'pending').where(["account_debited IN (?)",account_id]).order('updated_at desc').page(params[:page]).per(10)
   end
 
+  #FIXME_AB: As I said in for the above method user.vouchers.pending
   def get_pending_of_users(user_id)
     @vouchers = Voucher.where(workflow_state:'pending').where(creator_id: user_id).order('updated_at desc').page(params[:page]).per(10)
   end
-   def get_pending_by_date(to,from)
+
+  def get_pending_by_date(to,from)
     @vouchers = Voucher.where(workflow_state:'pending').where('date between (?) and (?)',to,from).order('updated_at desc').page(params[:page]).per(10)
   end
 
+  #FIXME_AB: Why you are using two types of method naming conventions. One with underscore other with camelCase
   def getAccount(id)
     @account = Account.find(id)
   end
   
+  #FIXME_AB: Better suites to User model
   def getUserNotifications
     notifications = PublicActivity::Activity.where('owner_id = ? and visited = false', current_user.id).order('id desc').count
     if notifications > 0
