3617e7479162be1cd2aba4cb5c8433d7f2d8b848
DivyaTalwar <divya@vinsol.com>
2013-11-27 20:18:17 +0530

    application fixes

--- app/models/account.rb
+++ app/models/account.rb
@@ -9,6 +9,8 @@ class Account < ActiveRecord::Base
   default_scope { order('name') }
   paginates_per 3
   #FIXME_AB: Good. But This will return false when I call obj.destroy. Lets do one more thing add error to the object :base. "We are not allowing destroy or delete for Account". Do the same thing for delete. since obj.delete will delete the object. delete don't fire callbacks to you would need to overwite delete method in he model
+  
+  before_validation :strip_blanks
   before_destroy do 
   	errors.add :base , "We are not allowing destroy or delete for Account" 
   	return false 
@@ -19,6 +21,9 @@ class Account < ActiveRecord::Base
   	return false 
   end
 
+  def strip_blanks
+    self.name = self.name.squish
+  end
 
   #FIXME_AB: Since this is needed for dropdown and not belongs to Account. Move it to constant.rb in initializers. And name it ACCOUNT_FILTER_OPTIONS or something similar
   #fixed
