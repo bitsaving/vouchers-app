5f3c89dc57222f793e6a981b063f3a3b29920e75
DivyaTalwar <divya@vinsol.com>
2013-11-29 17:40:26 +0530

    migration removed

--- db/migrate/20131118173617_rename_attachment.rb
+++ db/migrate/20131118173617_rename_attachment.rb
@@ -1,8 +1,8 @@
 class RenameAttachment < ActiveRecord::Migration
   def change
-  	rename_column :uploads , :avatar_file_name , :bill_attachment_file_name
-  	rename_column :uploads , :avatar_content_type , :bill_attachment_content_type
-  	rename_column :uploads , :avatar_file_size , :bill_attachment_file_size
-  	rename_column :uploads , :avatar_updated_at , :bill_attachment_updated_at
+  	# rename_column :uploads , :avatar_file_name , :bill_attachment_file_name
+  	# rename_column :uploads , :avatar_content_type , :bill_attachment_content_type
+  	# rename_column :uploads , :avatar_file_size , :bill_attachment_file_size
+  	# rename_column :uploads , :avatar_updated_at , :bill_attachment_updated_at
   end
 end
