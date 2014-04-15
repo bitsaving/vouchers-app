9b7795a1213f9ac24bb5bfb919f9476cf7b43e9d
DivyaTalwar <divya@vinsol.com>
2014-04-15 17:20:50 +0530

    fixes

--- app/mailers/voucher_notifier.rb
+++ app/mailers/voucher_notifier.rb
@@ -2,7 +2,6 @@ class VoucherNotifier < ActionMailer::Base
   def assigned_vouchers(user)
     @user = user
     if @user.assigned_vouchers.present?
-      #FIXME_AB: Here we have harcoded the email. Remember, how should we handle such situations?
       mail(:from => "voucherapp@vinsol.com", :to => @user.email, :subject => @user.assigned_vouchers.count.to_s + " vouchers assigned")
     end
   end
