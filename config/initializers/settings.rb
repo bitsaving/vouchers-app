36635b95faf3462341d8d1fbc8152b43fbfbe7c1
DivyaTalwar <divya@vinsol.com>
2014-04-16 11:26:16 +0530

    fixes

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,4 +1,4 @@
-# CREDENTIALS = YAML.load_file("#{Rails.root}/config/attachment.yml")[Rails.env]
-# BUCKET = CREDENTIALS["bucket"]
-# ACCESS_KEY = CREDENTIALS["access_key_id"]
-# SECRET_ACCESS = CREDENTIALS["secret_access_key"]
+CREDENTIALS = YAML.load_file("#{Rails.root}/config/attachment.yml")[Rails.env]
+BUCKET = CREDENTIALS["bucket"]
+ACCESS_KEY = CREDENTIALS["access_key_id"]
+SECRET_ACCESS = CREDENTIALS["secret_access_key"]
