927dd7e78e6f5d17d49d6eec32f4919e249a31b4
DivyaTalwar <divya@vinsol.com>
2014-06-30 16:41:42 +0530

    modified placement of files

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -18,13 +18,17 @@
     i = i+1;
     var new_id = new Date().getTime();
     var regexp = new RegExp("new_" + association, "g")
-    content = content.replace($(content).find('div').find('.autocomplete').val(), '')  
-    content = content.replace(/_\d+/g, function(val) {  return ("_" + (parseInt(val.split('_')[1], 10)+i).toString()) })
-    if($(content).find('div').find('.autocomplete').data('textbox-id'))
-      number  =  $(content).find('div').find('.autocomplete').data('textbox-id').split('_')[1]
-    content= content.replace(/\[new_transactions/g, function(val) { return ("[" + number.toString()) })
+    if(association == "attachments")
+      content = content.replace(/new_attachments/g, function(val) {  return $('.file-upload').size() + 1 })
+    else {    
+      content = content.replace($(content).find('div').find('.autocomplete').val(), '')  
+      content = content.replace(/_\d+/g, function(val) {  return ("_" + (parseInt(val.split('_')[1], 10)+i).toString()) })
+      if($(content).find('div').find('.autocomplete').data('textbox-id'))
+        number  =  $(content).find('div').find('.autocomplete').data('textbox-id').split('_')[1]
+      content= content.replace(/\[new_transactions/g, function(val) { return ("[" + number.toString()) })
+    }
     $(link).before(content.replace(regexp, $('.ui-autocomplete-input').size() + 1));
-    
+        // alert(content)
     
   }
   
